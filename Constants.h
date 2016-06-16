2ddd4addf6e603e1a91020b01ffdd235b214b994
Jared Moore <jmoore1@andrew.cmu.edu>
2016-06-16 19:18:52 -0400

    Cleaned up main.cpp and added global constants

---
+++ Constants.h
@@ -0,0 +1,9 @@
+#ifndef CONSTANTS_H
+#define CONSTANTS_H
+
+namespace Window {
+  const int WIDTH = 1280;
+  const int HEIGHT = 720;
+}
+
+#endif
--- main.cpp
+++ main.cpp
@@ -1,36 +1,11 @@
-#include <SFML/Audio.hpp>
 #include <SFML/Graphics.hpp>
 
+#include "Constants.h"
 #include "Game.h"
-#include "ResourceManager.h"
 
 int main()
 {
-    // Create the main window
-    sf::RenderWindow window(sf::VideoMode(800, 600), "SFML window");
-
-    ResourceManager resources;
-
-    /* sprite from SFML demo */
-    sf::Sprite sprite(resources.getTexture("cute_image.jpg"));
-
-    sf::Sprite blue(resources.getTexture("tileset.png"), {0, 0, 32, 32});
-    blue.setPosition(400.f, 400.f);
-
-    sf::Sprite red(resources.getTexture("tileset.png"), {32, 0, 32, 32});
-    red.setPosition(500.f, 500.f);
-
-    // Create a graphical text to display
-    /*sf::Font font;
-    if (!font.loadFromFile("arial.ttf"))
-        return EXIT_FAILURE;
-        sf::Text text("Hello SFML", font, 50);*/
-    // Load a music to play
-    sf::Music music;
-    /*if (!music.openFromFile("nice_music.ogg"))
-        return EXIT_FAILURE;
-    // Play the music
-    music.play();*/
+  sf::RenderWindow window(sf::VideoMode(Window::WIDTH, Window::HEIGHT), "Game Window");
 
     Game game(window);
 
@@ -45,22 +20,14 @@ int main()
             if (event.type == sf::Event::Closed)
                 window.close();
         }
-        // Clear screen
+
         window.clear();
 
         game.update();
         game.draw();
 
-        // Draw the sprite
-        /*
-        window.draw(sprite);
-        window.draw(red);
-        window.draw(blue);
-        */
-        // Draw the string
-        //window.draw(text);
-        // Update the window
         window.display();
     }
+
     return EXIT_SUCCESS;
 }
